2023bab029f35d524e7283b88a95e1e9bc2f36c7
ElPumpo <samo_kol@hotmail.com>
2019-03-05 19:46:39 +0100

    Proper indenting

--- server/main.lua
+++ server/main.lua
@@ -1,17 +1,17 @@
 ESX = nil
-TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
-
-local copsConnected 		= 0
-local robberPlayers 		= {}
+local copsConnected = 0
+local robberPlayers = {}
 local isCurrentlyRobbed = false
 
-function CountCops()
+TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
+function CountCops()
 	local xPlayers = ESX.GetPlayers()
 	copsConnected = 0
 
 	for i=1, #xPlayers, 1 do
 		local xPlayer = ESX.GetPlayerFromId(xPlayers[i])
+
 		if xPlayer.job.name == 'police' then
 			copsConnected = copsConnected + 1
 		end
@@ -23,11 +23,11 @@ function CountCops()
 end
 
 function AddMoneyToStores()
-
 	for _, v in pairs(Config.Zones) do
 		if v.MoneyRegeneration ~= nil then
 			local MaxMoney		= v.MaxMoney
 			local MoneyRegen	= v.MoneyRegeneration
+
 			if v.CurrentMoney < MaxMoney then
 				v.CurrentMoney = v.CurrentMoney + MoneyRegen
 			else
@@ -37,17 +37,17 @@ function AddMoneyToStores()
 	end
 
 	SetTimeout(Config.AddMoneyToStoresTimeOut * 60000, AddMoneyToStores)
-
 end
 
 function AddMoneyToBanks()
-
 	for _, v in pairs(Config.Zones) do
 		if v.MoneyRegeneration ~= nil then
 			local partOfCurrentMoney 	= ESX.Math.Round(v.CurrentMoney * Config.PercentCurrentMoney / 100)
 			local randomMoneyToBank 	= math.random(partOfCurrentMoney, partOfCurrentMoney * Config.MaxRandomMultiplier)
+
 			v.CurrentMoney = v.CurrentMoney - randomMoneyToBank
 			Config.Zones[v.BankToDeliver].CurrentMoney = Config.Zones[v.BankToDeliver].CurrentMoney + randomMoneyToBank
+
 			if Config.Zones[v.BankToDeliver].CurrentMoney > Config.Zones[v.BankToDeliver].MaxMoney then
 				Config.Zones[v.BankToDeliver].CurrentMoney = Config.Zones[v.BankToDeliver].MaxMoney
 			end
@@ -55,14 +55,13 @@ function AddMoneyToBanks()
 	end
 
 	SetTimeout(Config.AddMoneyToBanksTimeOut * 60000, AddMoneyToBanks)
-
 end
 
 RegisterServerEvent('esx_advanced_holdup:robberyCanceled')
 AddEventHandler('esx_advanced_holdup:robberyCanceled', function(zone)
-	local _source 	= source
-	local xPlayer		= ESX.GetPlayerFromId(_source)
-	local xPlayers 	= ESX.GetPlayers()
+	local _source  = source
+	local xPlayer  = ESX.GetPlayerFromId(_source)
+	local xPlayers = ESX.GetPlayers()
 
 	isCurrentlyRobbed = false
 
@@ -82,7 +81,6 @@ end)
 
 RegisterServerEvent('esx_advanced_holdup:robberyInProgress')
 AddEventHandler('esx_advanced_holdup:robberyInProgress', function(mainZone)
-
 	local _source 	= source
 	local xPlayer		= ESX.GetPlayerFromId(_source)
 	local xPlayers 	= ESX.GetPlayers()
@@ -116,11 +114,10 @@ AddEventHandler('esx_advanced_holdup:robberyInProgress', function(mainZone)
 			TriggerClientEvent('esx_advanced_holdup:startRobberingTimer', _source, mainZone)
 
 			Config.Zones[mainZone].Robbed = os.time()
-			robberPlayers[_source]	= mainZone
-			local savedSource 			= _source
+			robberPlayers[_source] = mainZone
+			local savedSource = _source
 
 			SetTimeout(zone.TimeToRob * 1000, function()
-
 				if robberPlayers[savedSource] then
 					isCurrentlyRobbed = false
 					if xPlayer then
@@ -136,7 +133,6 @@ AddEventHandler('esx_advanced_holdup:robberyInProgress', function(mainZone)
 						end
 					end
 				end
-
 			end)
 
 		else
@@ -144,7 +140,6 @@ AddEventHandler('esx_advanced_holdup:robberyInProgress', function(mainZone)
 		end
 
 	end
-
 end)
 
 CountCops()
